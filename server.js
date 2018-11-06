1c1332757af5351d1c4612d5bd37a871ab3d9234
hiwot nega <hiwotmanaye2@gmail.com>
2018-11-05 20:41:19 -0800

    registration and login form

--- server.js
+++ server.js
@@ -2,26 +2,29 @@
 
 const express = require('express');
 const pg = require('pg');
-require(`dotenv`).config();
 
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
-client.on('err', err => console.log(err));
 
 let app = express();
 app.set('view engine','ejs');
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`app is up on${PORT}`));
-
-app.get('/', loadBook);
 
 
-function loadBook(req, res) {
-  client.query('SELECT * FROM saved;')
-    .then( results => {
-      res.render('./index.ejs', {data: results.rows[0]});
-    })
+app.get('/',function(req, res){
+  res.send('it works!!');
+})
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use('/login',login);
+app.use('/sign-up',sign_up);
+function sign_up(req, res){
+  res.render('pages/registration');
 }
+function login(req, res){
+  res.render('pages/login');
+}
+const port = process.env.PORT || 8989;
+
+app.listen(port, () => console.log(`Server running on port:${port}`));
+ 
