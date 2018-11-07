3abdae6681da1b2e02a1e09ab6cb742577873bba
Charles Clemens <cclemensjr@gmail.com>
2018-11-07 15:14:33 -0800

    Commented out HTML5 drag n drop, added jQuery cdn to footer.ejs

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,30 @@
 'use strict';
 
-function dragstart_handler(event) {
-  console.log('In dragstart')
-  console.log('  ', event.target.id);
-  event.dataTransfer.setData('text/plain', event.target.id);
-  event.dropEffect = 'move';
-}
+// function dragstart_handler(event) {
+//   console.log('In dragstart')
+//   console.log('  ', event.target.id);
+//   event.dataTransfer.setData('text/plain', event.target.id);
+//   event.dropEffect = 'move';
+// }
 
-function dragover_handler(ev) {
-  console.log('In dragover')
-  event.preventDefault();
-  event.dataTransfer.dropEffect = 'move';
-}
+// function dragover_handler(event) {
+//   console.log('In dragover')
+//   event.preventDefault();
+//   event.dataTransfer.dropEffect = 'move';
+// }
+
+// function drop_handler(event) {
+//   console.log('In drop');
+//   event.preventDefault();
+//   let data = event.dataTransfer.getData('text/plain');
+//   let selected = document.getElementById(data);
+//   event.target.appendChild(document.getElementById(data));
+//   selected.style.position = 'absolute';
+//   selected.style.left = event.pageX - selected.offsetWidth / 2 + 'px';
+//   selected.style.top = event.pageY - selected.offsetHeight / 2 + 'px';
+
+
+// //   // grabMagnet(event,data);
+// }
+
 
