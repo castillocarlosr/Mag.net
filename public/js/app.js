5314c509d63c5d1c3d5da4b3a3f984a66724cff4
Charles Clemens <cclemensjr@gmail.com>
2018-11-07 14:15:53 -0800

    Moved scripts to footer so js can be used on every page

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,9 @@
 console.log('working');
 
 function dragstart_handler(event) {
+  console.log(event.target.id);
   event.dataTransfer.setData('text/plain', event.target.id);
   event.dropEffect = 'move';
 }
+
+
