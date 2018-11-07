0d967f333bc386c2a82800a2ab6f78a9850764f6
Charles Clemens <cclemensjr@gmail.com>
2018-11-07 14:32:18 -0800

    Added dragover_handler

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,15 @@
 'use strict';
 
 function dragstart_handler(event) {
-  console.log(event.target.id);
+  console.log('In dragstart')
+  console.log('  ', event.target.id);
   event.dataTransfer.setData('text/plain', event.target.id);
   event.dropEffect = 'move';
 }
 
+function dragover_handler(ev) {
+  console.log('In dragover')
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'move';
+}
 
