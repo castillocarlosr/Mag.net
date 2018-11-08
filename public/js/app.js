b2d5229580c96d61992df730f56e3d8a87829fe2
Charles Clemens <cclemensjr@gmail.com>
2018-11-07 16:35:14 -0800

    Added jquery draggable method to magnet objects

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,20 @@
 'use strict';
 
-// function dragstart_handler(event) {
-//   console.log('In dragstart')
-//   console.log('  ', event.target.id);
-//   event.dataTransfer.setData('text/plain', event.target.id);
-//   event.dropEffect = 'move';
-// }
-
-// function dragover_handler(event) {
-//   console.log('In dragover')
-//   event.preventDefault();
-//   event.dataTransfer.dropEffect = 'move';
-// }
-
-// function drop_handler(event) {
-//   console.log('In drop');
-//   event.preventDefault();
-//   let data = event.dataTransfer.getData('text/plain');
-//   let selected = document.getElementById(data);
-//   event.target.appendChild(document.getElementById(data));
-//   selected.style.position = 'absolute';
-//   selected.style.left = event.pageX - selected.offsetWidth / 2 + 'px';
-//   selected.style.top = event.pageY - selected.offsetHeight / 2 + 'px';
+$( function() {
+  console.log('In draggable');
+  $(".letterMag").draggable({
+    drag: function(event, ui) {
+      console.log(`EVENT: ${event.target.id} || UI ${ui.position.left}`);
 
 
-// //   // grabMagnet(event,data);
-// }
+    }
+  });
 
+  $(".memeMag").draggable({
+    
+  });
 
+  $(".wordMag").draggable({
+    
+  });
+});
