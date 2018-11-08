eb5988141e5a01259a2ab04bc3f862ec9b72ccef
Manaye <hiwotmanaye2@gmail.com>
2018-11-08 08:50:39 -0800

    humberger menu and some css

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('working');
+
+document.body.addEventListener('click', function(){
+  document.getElementById('sidebar').classList.toggle('active');
+  document.querySelector('.toggle-btn').classList.toggle('remove');
+})
+
+//   console.log('working');
+
